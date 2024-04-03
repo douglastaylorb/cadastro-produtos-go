f3a468724e1019d8ad044d8a27d14a66c8940f3c
Douglas Taylor <78963489+douglastaylorb@users.noreply.github.com>
2024-04-03 13:10:32 -0300

    Update db.go

--- src/db/db.go
+++ src/db/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ConectaComDb() *sql.DB {
-	conexao := "user=postgres dbname=registro_chamados password=douglastaylor#20 host=localhost sslmode=disable"
+	conexao := "user=postgres dbname=registro_chamados password=<senha_do_seu_banco_aqui> host=localhost sslmode=disable"
 	db, err := sql.Open("postgres", conexao)
 
 	if err != nil {
